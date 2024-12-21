3f0a76cedbd81ec0e0e4982f4103ec3b42b9cee6
pooriAbdollahnezhad <poori.Abdollahnezhad@gmail.com>
2024-12-21 15:02:16 +0330

    Update fetch_configs.py

--- src/fetch_configs.py
+++ src/fetch_configs.py
@@ -47,7 +47,7 @@ def fetch_wireguard_configs():
             return
         
         final_configs = [
-            f"{config}#Anon{i+1}"
+            f"{config}#IRANman{i+1}"
             for i, config in enumerate(configs)
         ]
         
